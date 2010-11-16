ff39119e7c63ba96a50ebca9ea769f53bb1e4240
martialstorm <martialstorm@76b46ade-9647-6af1-8dd8-1bc6abbb7144>
2010-11-16 15:42:48 +0000

    application build refactored. added task for creating test database sql script is split and moved to the "sql" directory

--- src/test/com/ast/kinoshka/backend/service/util/FileUtilTest.java
+++ src/test/com/ast/kinoshka/backend/service/util/FileUtilTest.java
@@ -1,5 +1,12 @@
 package com.ast.kinoshka.backend.service.util;
 
+import org.junit.Test;
+import org.junit.Ignore;
+
 public class FileUtilTest {
 
+	@Test
+	@Ignore
+	public void testSmth() {
+	}
 }
--- src/test/com/ast/kinoshka/testcommon/BaseServiceTest.java
+++ src/test/com/ast/kinoshka/testcommon/BaseServiceTest.java
@@ -7,10 +7,12 @@ import com.google.inject.Injector;
 
 public abstract class BaseServiceTest {
 
+  public final static String WEB_CONTEXT = "target/test/web";
+  public final static String DB_CONTEXT = "target/db";
   protected Injector injector;
 
   public BaseServiceTest() {
-    injector = Guice.createInjector(new AppConfigModule("test/web", "test/db/storage", 0),
+    injector = Guice.createInjector(new AppConfigModule(WEB_CONTEXT, DB_CONTEXT, 0),
         new CoreServiceModule()); 
   }
 
